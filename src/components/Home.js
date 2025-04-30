33aea526b4cf6fd835b57e4b6fcf9ef649a5afd0
lekden <lekden7869@gmail.com>
2025-04-30 12:15:25 +1000

    UI Improvement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,55 @@
-import React from 'react';
-import './Home.css'; // Import the CSS file
-import HomeGallery from './HomeGallery'; // Import the HomeGallery component
-import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
-import Banner from './Banner';
-
-
+import React from "react";
+import "./Home.css"; // Import the CSS file
+import HomeGallery from "./HomeGallery"; // Import the HomeGallery component
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
+import Banner from "./Banner";
 
 const Home = () => {
-    console.log("Home component rendered");
-    const navigate = useNavigate(); // Initialize the navigate function
-    return (
-        <div>   <Banner />
-            <div className='wrapper'>
-                <div className='home-info'>
-                    <div className="moving-text">
-                        We are registered with APTO - Assoication of Bhutanese Guide Operators, approved by TCB.
-                    </div>
-                    <div className="info-container">
-                        <div className='welcome-container'>
-                            <div className="welcome-text1">
-                                <span className="welcome-heading">Whats in store?</span>
-                                <br />We are a travel agency with over 20 years of experience dedicated to providing you with the best travel experiences.
-                                Our team of experts is here to help you plan your next adventure.</div>
+  console.log("Home component rendered");
+  const navigate = useNavigate(); // Initialize the navigate function
+  return (
+    <div>
+      <Banner />
+      <div className="wrapper">
+        <div className="home-info">
+          <div className="moving-text">
+            We are registered with APTO - Assoication of Bhutanese Guide
+            Operators, approved by TCB.
+          </div>
 
-                            <div className="welcome-text">Whether you're looking for a relaxing vacation, an adventurous trek in the mountains,
-                                or a cultural experience, our job is to get things working for you. Leave the undesirable job of planning and organizing to us!</div>
+          <div className="info-container">
+            <div className="welcome-container">
+              <div className="welcome-text1">
+                <span className="welcome-heading">Whats in store?</span>
+                <br />
+                We are a travel agency with over 20 years of experience
+                dedicated to providing you with the best travel experiences. Our
+                team of experts is here to help you plan your next adventure.
+              </div>
 
-                            <br />
-                            <div className='info-table' >Currency info</div>
-                            <iframe src=" " title="Embedded Content" width="300" height="300"></iframe>
-                        </div>
+              <div className="welcome-text">
+                Whether you're looking for a relaxing vacation, an adventurous
+                trek in the mountains, or a cultural experience, our job is to
+                get things working for you. Leave the undesirable job of
+                planning and organizing to us!
+              </div>
 
-                        <HomeGallery />
-                    </div>
+              <br />
+              <div className="info-table">Currency info</div>
+              <iframe
+                src=" "
+                title="Embedded Content"
+                width="300"
+                height="300"
+              ></iframe>
+            </div>
 
-                </div>
-            </div >
+            <HomeGallery />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Knowus.js
+++ src/components/Knowus.js
@@ -1,53 +1,66 @@
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import './Home.css';
-import './Knowus.css';
+import { Link } from "react-router-dom";
+import "./Home.css";
+import "./Knowus.css";
 import { AiFillHome } from "react-icons/ai";
-import Banner from './Banner';
+import Banner from "./Banner";
 
 export default function Knowus() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <Banner />
-            <div className="knowus-container">
-                <div className="knowus">
-                    <h1 className="knowus-title">Know Us</h1>
-                    <p>About Melong Tours & Treks
-                        With over 20 years of experience, Melong Tours & Treks has been proudly guiding
-                        travelers through the breathtaking landscapes of Bhutan.
-                        From cultural journeys to adventurous treks, we handle every detail
-                        so you can simply enjoy the experience. Let us do the hard work — planning,
-                        organizing, and fine-tuning — while you focus on creating unforgettable memories.
-                    </p><br />
-                    <h3>Meet our team</h3>
-                    <div className="knowus-intro">
-                        <div>
-                            <strong>Passang Singye<br />
-                                Founder & Proprietor </strong><br /><br />
-                            A passionate traveler
-                            with a deep love for Bhutan’s culture and nature. With decades of experience in
-                            the travel industry, he has built Melong on a foundation of trust, expertise, and
-                            personal care. <br />
-                            Our General Manager brings a wealth of operational experience,
-                            ensuring that every tour runs smoothly and every guest feels at home. Together,
-                            they lead a team dedicated to making your journey through Bhutan seamless, memorable,
-                            and truly special.</div><br />
-                        <div>
-                            <strong> Sonam Tshering<br />
-                                Head Guide</strong><br /><br />
-                            With over 15 years of experience in guiding tours and treks across Bhutan,
-                            our Head Guide is a passionate expert dedicated to offering you an unforgettable
-                            adventure. Highly knowledgeable about Bhutan’s rich culture, history, and
-                            landscapes, they ensure every journey is safe, informative, and enjoyable.
-                            Their leadership and commitment to quality service make them an integral part of
-                            the team, helping travelers experience the true essence of Bhutan.
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div>
+      <Banner />
+      <div className="knowus">
+        <h1 className="knowus-title">Know Us</h1>
+        <p>
+          About Melong Tours & Treks With over 20 years of experience, Melong
+          Tours & Treks has been proudly guiding travelers through the
+          breathtaking landscapes of Bhutan. From cultural journeys to
+          adventurous treks, we handle every detail so you can simply enjoy the
+          experience. Let us do the hard work — planning, organizing, and
+          fine-tuning — while you focus on creating unforgettable memories.
+        </p>
+        <br />
+        <h3>Meet our team</h3>
+        <div className="knowus-intro">
+          <div>
+            <strong>
+              Passang Singye
+              <br />
+              Founder & Proprietor{" "}
+            </strong>
+            <br />
+            <br />
+            A passionate traveler with a deep love for Bhutan’s culture and
+            nature. With decades of experience in the travel industry, he has
+            built Melong on a foundation of trust, expertise, and personal care.{" "}
+            <br />
+            Our General Manager brings a wealth of operational experience,
+            ensuring that every tour runs smoothly and every guest feels at
+            home. Together, they lead a team dedicated to making your journey
+            through Bhutan seamless, memorable, and truly special.
+          </div>
+          <br />
+          <div>
+            <strong>
+              {" "}
+              Sonam Tshering
+              <br />
+              Head Guide
+            </strong>
+            <br />
+            <br />
+            With over 15 years of experience in guiding tours and treks across
+            Bhutan, our Head Guide is a passionate expert dedicated to offering
+            you an unforgettable adventure. Highly knowledgeable about Bhutan’s
+            rich culture, history, and landscapes, they ensure every journey is
+            safe, informative, and enjoyable. Their leadership and commitment to
+            quality service make them an integral part of the team, helping
+            travelers experience the true essence of Bhutan.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
